a12777eeccf321d4231ef40b7dd885a0c6ffc3f4
Alwin Arrasyid <alwin.ridd@gmail.com>
2017-12-09 03:22:23 +0700

    Initial commit.

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, send_file
+
+app = Flask(__name__, static_folder='build/static')
+
+@app.route("/")
+def index():
+    return send_file("build/index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
